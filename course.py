78a62c7e374be1918966069c08592f26f8479b99
raghavshrm987 <18ucc156@lnmiit.ac.in>
2019-02-12 23:40:11 +0530

    basic python programm

---
+++ course.py
@@ -0,0 +1,27 @@
+course=['history','mathematics','physics','compsci']
+course_2=['art','education']
+
+num = [1,2,3,4,5]    #can do this
+
+# course.append(course_2)
+# course.extend(course_2)
+# course.remove(mathematics)
+#popped = course.pop()
+#course.pop()
+# course.reverse()
+course.sort(reverse = False)
+sorted_course=sorted(course)# without changing the order
+
+
+
+# for item in course:
+print(course)
+
+# we can use any thing in place of item it show same result
+
+# print('art' in course)
+
+# print(course.index('compsci'))
+
+# print(sum(num))
+# msx/min(num)
