a41b30d15a5c82e97644927bd22c18d251a3d5b1
raghavshrm987 <18ucc156@lnmiit.ac.in>
2019-02-14 00:56:52 +0530

    basic programm of ste,tuples and list in python

---
+++ sets.py
@@ -0,0 +1,8 @@
+cs_course = {'history','Math','physics','copmsci'}
+art_course = {'history','Math','fashion','design'}
+
+print(cs_course.intersection(art_course))
+print(cs_course.difference(art_course))
+print(cs_course.union(art_course))
+
+print('Math' in cs_course)  #can do in all three set,list,tuples
